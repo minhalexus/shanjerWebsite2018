01aeb8d1a1cf5f25cddc3c5383d999a60a53ebbc
Minhal <mshanjer@gmail.com>
2018-07-22 15:10:54 -0400

    implemented basic bootstrap cover template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home', to: 'project#index'
+  root to: 'project#index'
 end
